62dec20ade4c654a992927cc956e8cda7803e9dc
gokisri <gokilaselvaraj2020@gmail.com>
2024-09-12 23:04:13 +0530

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,3 +1,10 @@
 data = [10, 501, 22, 37, 100, 999, 87, 351]
 result = filter(lambda x: x > 4, data)
 print(list(result))
+
+
+
+output:
+[10, 501, 22, 37, 100, 999, 87, 351]
+
+The filter function applies the lambda function lambda x: x > 4 to each element of the data list. Since all the elements in the list are greater than 4, none of them are filtered out.
