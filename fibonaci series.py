4d6c052aff560b988cf21255604b92851d9114cf
gokisri <gokilaselvaraj2020@gmail.com>
2024-09-12 23:01:34 +0530

    Update fibonaci series.py

--- fibonaci series.py
+++ fibonaci series.py
@@ -1,17 +1,9 @@
 from functools import reduce
 
-# Lambda function to generate Fibonacci sequence
-fibonacci_series = lambda n: reduce(lambda x, _: x + [x[-1] + x[-2]], range(n - 2), [0, 1])
+# Lambda function to generate Fibonacci sequence starting from 1
+fibonacci_series = lambda n: reduce(lambda x, _: x + [x[-1] + x[-2]], range(n - 2), [1, 1])
 
-# Generate first 50 Fibonacci numbers
+# Generate and print the first 50 Fibonacci numbers
 fib_50 = fibonacci_series(50)
+print(fib_50)
 
-# Display the result
-fib_50
-fibonacci_series_from_1 = lambda n: reduce(lambda x, _: x + [x[-1] + x[-2]], range(n - 2), [1, 1])
-
-# Generate first 50 Fibonacci numbers starting from 1
-fib_50_from_1 = fibonacci_series_from_1(50)
-
-# Display the result
-fib_50_from_1
